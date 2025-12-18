b198a08c385f04ae5e41def1c00835d3ccc6ca5a
ilzavfxx-hash <ilzavfxx@gmail.com>
2025-12-18 22:07:25 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Load the React app from the dist folder after Vite build
+  win.loadFile(path.join(__dirname, 'dist', 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
